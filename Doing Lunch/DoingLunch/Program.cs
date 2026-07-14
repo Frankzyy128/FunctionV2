d421da1ae956cd5ab5b9bce7f111ace5e74e4e93
Liang <Leon.tang67@gmail.com>
2026-07-14 14:23:56 +0100

    Add GetInteger method to reduce code duplication

--- Doing Lunch/DoingLunch/Program.cs
+++ Doing Lunch/DoingLunch/Program.cs
@@ -16,18 +16,35 @@ namespace DoingLunch
             Console.WriteLine("Hello, your Lunch is " + mainCourse);
             Console.WriteLine("Hello, your lunch is {0}", mainCourse);
             Console.WriteLine("How many roast potatoes?");
-            string sRoasties;
-            sRoasties = Console.ReadLine();
-            int roastCount;
-            roastCount = Convert.ToInt32(sRoasties);
-            Console.WriteLine("Hello your lunch is " + mainCourse + " with " + roastCount + " roast potatoes ");
-      
-           
-            //Console.WriteLine("How many brussel sprouts?");
+            //string sRoasties;
+            //sRoasties = Console.ReadLine();
+            //int roastCount;
+            //roastCount = Convert.ToInt32(sRoasties);
+            //Console.WriteLine("Hello your lunch is " + mainCourse + " with " + roastCount + " roast potatoes ");
 
+
+            //Console.WriteLine("How many brussel sprouts?");
+            //string sBruSprouts;
+            //sBruSprouts = Console.ReadLine();
+            //int sproutsCount;
+            //sproutsCount = Convert.ToInt32(sBruSprouts);
+            //Console.WriteLine("Hello your lunch is " + mainCourse + " with " + sproutsCount + " Brussel Sprouts ");
 
             // Put the display here!
+            int roastCount = GetInteger(Console.ReadLine());
+            Console.WriteLine("Hello your lunch is " + mainCourse + " with " + roastCount + " roast potatoes ");
 
+            Console.WriteLine("How many brussel sprouts?");
+            int sproutsCount = GetInteger(Console.ReadLine());
+            Console.WriteLine("Hello your lunch is " + mainCourse + " with " + sproutsCount + " Brussel Sprouts ");
+        }
+
+
+        static int GetInteger (string message) {
+            //int numOfCourses;
+            //numOfCourses = Convert.ToInt32 (message);
+            //return numOfCourses;
+            return Convert.ToInt32(message);
         }
 
     }
